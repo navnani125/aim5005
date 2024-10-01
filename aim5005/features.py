700d5c96a341ce180a7db9038e4e8c5d3cb0032e
navnani125 <76695819+navnani125@users.noreply.github.com>
2024-09-30 23:31:22 -0400

    Update features.py
    correcting the MinMaxScaler function

--- aim5005/features.py
+++ aim5005/features.py
@@ -7,37 +7,29 @@ class MinMaxScaler:
         self.minimum = None
         self.maximum = None
         
-    def _check_is_array(self, x:np.ndarray) -> np.ndarray:
-        """
-        Try to convert x to a np.ndarray if it'a not a np.ndarray and return. If it can't be cast raise an error
-        """
+    def _check_is_array(self, x: np.ndarray) -> np.ndarray:
         if not isinstance(x, np.ndarray):
             x = np.array(x)
-            
-        assert isinstance(x, np.ndarray), "Expected the input to be a list"
+        assert isinstance(x, np.ndarray), "Expected the input to be a list or np.ndarray"
         return x
-        
     
-    def fit(self, x:np.ndarray) -> None:   
+    def fit(self, x: np.ndarray) -> None:
         x = self._check_is_array(x)
-        self.minimum=x.min(axis=0)
-        self.maximum=x.max(axis=0)
+        self.minimum = x.min(axis=0)
+        self.maximum = x.max(axis=0)
         
-    def transform(self, x:np.ndarray) -> list:
+    def transform(self, x: np.ndarray) -> np.ndarray:
         """
         MinMax Scale the given vector
         """
         x = self._check_is_array(x)
         diff_max_min = self.maximum - self.minimum
-        
-        # TODO: There is a bug here... Look carefully! 
-        return x-self.minimum/(self.maximum-self.minimum)
+        return (x - self.minimum) / diff_max_min  # Fix: element-wise division
     
-    def fit_transform(self, x:list) -> np.ndarray:
+    def fit_transform(self, x: list) -> np.ndarray:
         x = self._check_is_array(x)
         self.fit(x)
-        return self.transform(x)
-    
+        return self.transform(x)    
     
 class StandardScaler:
     def __init__(self):
