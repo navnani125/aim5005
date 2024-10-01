0556698da626bafc53548444e323eb2e12ae0404
navnani125 <76695819+navnani125@users.noreply.github.com>
2024-09-30 23:29:00 -0400

    Update evaluation.py
    adding the predict function

--- aim5005/evaluation.py
+++ aim5005/evaluation.py
@@ -1,29 +1,35 @@
 import numpy as np
-from typing import List, Tuple
+from typing import List
 
-def error(intercept:float, beta:float, x_i: float, y_i:float)->float:
+def predict(intercept: float, beta: float, x_i: float) -> float:
     """
-    Find the difference between the predicted value and the actual value, where actual is is y_i
+    Predicts y value based on intercept, beta (slope), and x value.
     """
-    return predict_(intercept, beta, x_i) - y_i
+    return intercept + beta * x_i
 
-def sum_of_square_error(intercept:float, beta:float, x: List[float], y_actual: List[float]) -> float:
+def error(intercept: float, beta: float, x_i: float, y_i: float) -> float:
     """
-    Square the errors and sum them (we don't want errors to cancel if one is positive and one is negative)
+    Find the difference between the predicted value and the actual value, where actual is y_i.
     """
-    return np.sum([error_(intercept, beta, x_i, y_i) ** 2 for x_i, y_i in zip(x, y)])
+    return predict(intercept, beta, x_i) - y_i
+
+def sum_of_square_error(intercept: float, beta: float, x: List[float], y_actual: List[float]) -> float:
+    """
+    Square the errors and sum them.
+    """
+    return np.sum([error(intercept, beta, x_i, y_i) ** 2 for x_i, y_i in zip(x, y_actual)])
 
 def total_sum_of_squares(y_actual: List[float]) -> float:
     """
-    Get the SST
+    Calculate the total sum of squares (SST).
     """
     meanval = np.mean(y_actual)
-    return sum((y - meanval)**2)
+    return np.sum([(y - meanval) ** 2 for y in y_actual])
 
-def rsquared(intercept:float, coef:float, x:List[float], y_actual:List[float]) -> float:
+def rsquared(intercept: float, coef: float, x: List[float], y_actual: List[float]) -> float:
     """
     R^2 = 1 - (SSE/SST)
     """
-    sse = sum_of_square_error_(intercept=intercept, beta=coef, x=x, y_actual=y_actual) 
-    sst = total_sum_of_squares(y_actual=y_actual)
-    return 1 - (sse/sst)
+    sse = sum_of_square_error(intercept, coef, x, y_actual)
+    sst = total_sum_of_squares(y_actual)
+    return 1 - (sse / sst)
