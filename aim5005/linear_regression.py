c192ef5328f885e24dc128fe067b316173ffbf23
navnani125 <76695819+navnani125@users.noreply.github.com>
2024-09-30 23:41:26 -0400

    Update linear_regression.py
    correcting a typo

--- aim5005/linear_regression.py
+++ aim5005/linear_regression.py
@@ -2,21 +2,22 @@ import numpy as np
 from typing import List, Tuple
 from aim5005.prep import correlation
 
-class LinearRegresion:
+class LinearRegression:
     def __init__(self):
         self.intercept = None
         self.beta = None
     
-    def predict(self, x:List[float]) -> float:
-        """ Given values, x, predict the y value
+    def predict(self, x: List[float]) -> np.ndarray:
+        """
+        Given values x, predict the y value.
         """
         return self.intercept + self.beta * np.array(x)
     
-    def fit(self, x: List[float], y:List[float]) -> None:
+    def fit(self, x: List[float], y: List[float]) -> None:
         self.intercept, self.beta = self.least_squares_fit(x, y)
     
-    def least_squares_fit(self, x: List[float], y:List[float]) -> Tuple[float, float]:
+    def least_squares_fit(self, x: List[float], y: List[float]) -> Tuple[float, float]:
         x, y = np.array(x), np.array(y)
-        beta = correlation(x,y) * np.std(y)/ np.std(x)  
+        beta = correlation(x, y) * np.std(y) / np.std(x)
         intercept = np.mean(y) - beta * np.mean(x)
-        return (intercept, beta)
+        return intercept, beta
