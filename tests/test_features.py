34aefc7b47429f29737f2713c31dd82794c6a8be
navnani125 <76695819+navnani125@users.noreply.github.com>
2024-09-30 23:39:25 -0400

    Update test_features.py
    adding label encoder test cases

--- tests/test_features.py
+++ tests/test_features.py
@@ -1,4 +1,4 @@
-from aim5005.features import MinMaxScaler, StandardScaler
+from aim5005.features import MinMaxScaler, StandardScaler, LabelEncoder
 import numpy as np
 import unittest
 from unittest.case import TestCase
@@ -10,14 +10,12 @@ class TestFeatures(TestCase):
         scaler = MinMaxScaler()
         assert isinstance(scaler, MinMaxScaler), "scaler is not a MinMaxScaler object"
         
-        
     def test_min_max_fit(self):
         scaler = MinMaxScaler()
         data = [[-1, 2], [-0.5, 6], [0, 10], [1, 18]]
         scaler.fit(data)
         assert (scaler.maximum == np.array([1., 18.])).all(), "scaler fit does not return maximum values [1., 18.] "
-        assert (scaler.minimum == np.array([-1., 2.])).all(), "scaler fit does not return maximum values [-1., 2.] " 
-        
+        assert (scaler.minimum == np.array([-1., 2.])).all(), "scaler fit does not return minimum values [-1., 2.] " 
         
     def test_min_max_scaler(self):
         data = [[-1, 2], [-0.5, 6], [0, 10], [1, 18]]
@@ -51,6 +49,7 @@ class TestFeatures(TestCase):
         data = [[0, 0], [0, 0], [1, 1], [1, 1]]
         expected = np.array([[-1., -1.], [-1., -1.], [1., 1.], [1., 1.]])
         scaler.fit(data)
+        result = scaler.transform(data)
         assert (result == expected).all(), "Scaler transform does not return expected values. Expect {}. Got: {}".format(expected.reshape(1,-1), result.reshape(1,-1))
         
     def test_standard_scaler_single_value(self):
@@ -61,7 +60,32 @@ class TestFeatures(TestCase):
         result = scaler.transform([[2., 2.]]) 
         assert (result == expected).all(), "Scaler transform does not return expected values. Expect {}. Got: {}".format(expected.reshape(1,-1), result.reshape(1,-1))
 
-    # TODO: Add a test of your own below this line
-    
+    # New tests for LabelEncoder
+    def test_label_encoder_init(self):
+        encoder = LabelEncoder()
+        assert isinstance(encoder, LabelEncoder), "encoder is not a LabelEncoder object"
+        
+    def test_label_encoder_fit(self):
+        encoder = LabelEncoder()
+        data = ["cat", "dog", "cat", "bird"]
+        encoder.fit(data)
+        expected_classes = np.array(["bird", "cat", "dog"])
+        assert (encoder.classes_ == expected_classes).all(), "LabelEncoder fit does not return expected classes."
+        
+    def test_label_encoder_transform(self):
+        encoder = LabelEncoder()
+        data = ["cat", "dog", "cat", "bird"]
+        encoder.fit(data)
+        result = encoder.transform(["cat", "dog", "bird", "cat"])
+        expected = np.array([1, 2, 0, 1])  # Assuming 'bird' -> 0, 'cat' -> 1, 'dog' -> 2
+        assert (result == expected).all(), "LabelEncoder transform does not return expected values. Expect {}. Got: {}".format(expected, result)
+        
+    def test_label_encoder_fit_transform(self):
+        encoder = LabelEncoder()
+        data = ["cat", "dog", "cat", "bird"]
+        result = encoder.fit_transform(data)
+        expected = np.array([1, 2, 1, 0])  # Based on the previous assumptions
+        assert (result == expected).all(), "LabelEncoder fit_transform does not return expected values. Expect {}. Got: {}".format(expected, result)
+
 if __name__ == '__main__':
     unittest.main()
